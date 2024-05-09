53492ec8c6c6c619094d975f4fa5a081c9b590da
kyoungmin <stephen4951@gmail.com>
2024-05-09 16:47:44 +0900

    할 일 체크 기능, 날짜 렌더링

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -69,10 +69,39 @@ const TodoTemplate = () => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  // 할 일 체크 처리 함수
+  const checkTodo = (id) => {
+    /*
+    const copyTodos = [...todos];
+    for (let cTodo of copyTodos) {
+      if (cTodo.id === id) {
+        cTodo.done = !cTodo.done;
+      }
+    }
+    setTodos(copyTodos);
+    */
+
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, done: !todo.done }
+          : todo,
+      ),
+    );
+  };
+
+  // 체크가 안 된 할 일의 개수를 카운트 하기
+  const countRestTodo = () =>
+    todos.filter((todo) => !todo.done).length;
+
   return (
     <div className='TodoTemplate'>
-      <TodoHeader />
-      <TodoMain todoList={todos} remove={removeTodo} />
+      <TodoHeader count={countRestTodo} />
+      <TodoMain
+        todoList={todos}
+        remove={removeTodo}
+        check={checkTodo}
+      />
       <TodoInput addTodo={addTodo} />
     </div>
   );
