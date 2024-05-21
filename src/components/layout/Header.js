337b985fff296dd707fcfecd2bcf3be6f7069914
kyoungmin <stephen4951@gmail.com>
2024-05-21 16:01:06 +0900

    Todo 관련 요청에 토큰 전달 로직, 로그인 여부에 따른 Header 렌더링

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,7 +4,7 @@ import {
   Toolbar,
   Typography,
 } from '@mui/material';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../../scss/Header.scss';
 import AuthContext from '../../utils/AuthContext';
